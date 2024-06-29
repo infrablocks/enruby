7cfd1f318e2363c1a3734e2f1594765c75fb4cb2
Toby Clemson <tobyclemson@gmail.com>
2024-06-29 17:32:25 +0100

    Initial commit.

---
+++ spec/enruby_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Enruby do
+  it 'has a version number' do
+    expect(Enruby::VERSION).not_to be_nil
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'simplecov'
+
+SimpleCov.start do
+  enable_coverage :branch
+  add_filter '/spec/'
+end
+
+require 'bundler/setup'
+require 'faker'
+
+require 'enruby_spec'
+
+Dir[File.join(__dir__, 'support', '**', '*.rb')]
+  .each { |f| require f }
+
+RSpec.configure do |config|
+  config.example_status_persistence_file_path = '.rspec_status'
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
